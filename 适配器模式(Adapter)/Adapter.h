3f3e1abdb33049bd0377e0e8a9e71b24d6998651
chenbei97 <chenbei_electric@163.com>
2023-06-20 17:29:03 +0800

    适配器模式 - 程序

---
+++ 适配器模式(Adapter)/Adapter.h
@@ -0,0 +1,43 @@
+/*
+ * @Author: chenbei97 chenbei_electric@163.com
+ * @Date: 2023-06-20 17:06:26
+ * @LastEditors: chenbei97 chenbei_electric@163.com
+ * @LastEditTime: 2023-06-20 17:25:22
+ * @FilePath: \CPPDesignMode\适配器模式(Adapter)\Adapter.h
+ * @Description: 适配器模式
+ */
+#ifndef Adapter_H
+#define Adapter_H
+#include "stdio.h"
+
+class LogToFile {
+    public:
+        void readFile(){};
+        void writeFile(){};
+};
+
+class LogToDB {
+    public: 
+        virtual void readDB(){printf("read file!\n");};
+        virtual void writeDB(){printf("write file!\n");};
+};
+
+class LogAdapter : public LogToDB{
+    // 注意: 是继承新接口
+    private:
+        LogToFile * mFile; // 老接口的指针
+    public:
+        LogAdapter(LogToFile* file):mFile(file){}
+        void readDB() override {mFile->readFile();} // 新接口内部调用的老接口
+        void writeDB() override {mFile->writeFile();}
+};
+
+void test()
+{
+    LogToFile file;
+    LogAdapter ada(&file);
+    printf("----\n");
+    ada.readDB();
+    ada.writeDB();
+}
+#endif
---
+++ 适配器模式(Adapter)/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * @Author: chenbei97 chenbei_electric@163.com
+ * @Date: 2023-06-20 17:06:19
+ * @LastEditors: chenbei97 chenbei_electric@163.com
+ * @LastEditTime: 2023-06-20 17:25:17
+ * @FilePath: \CPPDesignMode\适配器模式(Adapter)\main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include "Adapter.h"
+
+int main()
+{
+    test();
+    return 0;
+}
+/*
+cd C:\Users\22987\Desktop\CPPDesignMode\适配器模式(Adapter)
+g++ main.cpp -o main
+.\main
+*/
