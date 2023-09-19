eba945426f938ef4729cfb52f4d64cd87c368743
chenbei97 <chenbei_electric@163.com>
2023-09-19 09:20:28 +0800

    备忘录模式

--- 备忘录模式(Memento)/Memento.h
+++ 备忘录模式(Memento)/Memento.h
@@ -2,13 +2,51 @@
  * @Author: chenbei97 chenbei_electric@163.com
  * @Date: 2023-09-18 16:23:18
  * @LastEditors: chenbei97 chenbei_electric@163.com
- * @LastEditTime: 2023-09-18 16:27:30
+ * @LastEditTime: 2023-09-19 09:14:14
  * @FilePath: \CPPDesignMode\备忘录模式(Memento)\Memento.h
- * @Copyright (c) 2023 by ${chenbei}, All Rights Reserved. 
+ * @Copyright (c) 2023 by ${chenbei}, All Rights Reserved.  
  */
 
 #ifndef Memento_H
 #define Memento_H
+#include <string>
+using std::string;
+
+class Fighter;
+class Memento
+{
+public:
+    void setAge(int age){mage = age;};
+    void setName(string name){mname = name;};
+    void display(){
+        printf("age = %d,name = %s\n",mage,mname.c_str());
+    }
+private:
+    friend class Fighter;
+    Memento(int age,string name):mage(age),mname(name){}
+    int mage;
+    string mname;
+};
+
+class Fighter{
+    public:
+        Fighter(int age,string name):mage(age),mname(name){}
+        void setAge(int age){mage = age;};
+        void setName(string name){mname = name;};
+        void display(){
+            printf("age = %d,name = %s\n",mage,mname.c_str());
+        }
+        Memento* createMemento() {
+            return new Memento(mage,mname);
+        }
+        void restoreMemento(Memento* m) {
+            mage = m->mage;
+            mname = m->mname;
+        }
+    private:
+        int mage;
+        string mname;
+};
 
 
 #endif
--- 备忘录模式(Memento)/main.cpp
+++ 备忘录模式(Memento)/main.cpp
@@ -2,7 +2,7 @@
  * @Author: chenbei97 chenbei_electric@163.com
  * @Date: 2023-09-18 16:23:18
  * @LastEditors: chenbei97 chenbei_electric@163.com
- * @LastEditTime: 2023-09-18 16:24:38
+ * @LastEditTime: 2023-09-19 09:09:28
  * @FilePath: \CPPDesignMode\备忘录模式(Memento)\main.cpp
  * @Copyright (c) 2023 by ${chenbei}, All Rights Reserved. 
  */
@@ -10,7 +10,10 @@
 
 int main()
 {
-
+    Fighter * fighter = new Fighter(1,"123");
+    fighter->display();
+    auto m = fighter->createMemento();
+    m->display();
     return 0;
 }
 /*
